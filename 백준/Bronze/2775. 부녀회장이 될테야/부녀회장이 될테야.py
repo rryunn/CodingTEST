31d30c10b635f979e456304d2128762a0d9ed522
rryunn <122458948+rryunn@users.noreply.github.com>
2025-02-04 22:31:06 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -0,0 +1,13 @@
+
+T = int(input())
+
+for _ in range(T):
+    k = int(input())
+    n = int(input())
+    people = [i for i in range(1,n+1)]
+    
+    
+    for _ in range(k):
+        for y in range(1, n):
+            people[y] += people[y-1] 
+    print(people[-1])
