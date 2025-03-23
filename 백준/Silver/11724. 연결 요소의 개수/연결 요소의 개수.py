386f4d4ef5247e1e60357f07bbe9d4fc034e5d44
rryunn <122458948+rryunn@users.noreply.github.com>
2025-03-23 17:49:49 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 436 ms, Memory: 176400 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,25 +1,22 @@
-def dfs(start):
-    visited[start] = True
+def dfs(node):
+    visited.add(node)
+    for neighbor in graph[node]:
+        if neighbor not in visited:
+            dfs(neighbor)
+            
+V,E = map(int,input().split())
+graph = [[] for _ in range(V+1)]
 
-    for i in graph[start]:
-        if not visited[i]:
-            dfs(i)
-    
-    
-N,M = map(int,input().split())
-graph = [[] for _ in range(N+1)]
+for _ in range(E):
+    u,v = map(int,input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+visited = set()
+count = 0
 
-for i in range(M):
-    line1, line2 = map(int,input().split())
-    graph[line1].append(line2)
-    graph[line2].append(line1)
-    
-count = 0 
-visited = [False]*(N+1)
-
-for i in range(1, N+1):
-    if not visited[i]:
-        dfs(i)
+for node in range(1,V+1):
+    if node not in visited:
+        dfs(node)
         count+=1
         
 print(count)
