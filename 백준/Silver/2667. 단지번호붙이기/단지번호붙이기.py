c2cca889279be6e5a1ec093fc4225f76832103d5
rryunn <122458948+rryunn@users.noreply.github.com>
2025-03-28 17:39:48 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 32488 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,42 +1,38 @@
-
-from collections import deque
-n = int(input())
-
-lst = [list(map(int,input())) for _ in range(n)]   
-def bfs( start_x, start_y):
-    count = 1
-    queue = deque()
-    queue.append([start_x,start_y])
-    lst[start_x][start_y] = 0
-    dx = [-1,1,0,0]
-    dy = [0,0, -1,1]
-
-    while queue:
-        x,y = queue.popleft()
-        
-        for d in range(4):
-            nx = x + dx[d]
-            ny = y + dy[d]
-
-            if (0<= nx < n) and (0<= ny < n) and lst[nx][ny] == 1:
-                lst[nx][ny] = 0
-                queue.append((nx,ny))
-            
-                count += 1
-            
-    return count
-        
-
-
-cnt = []
-
-for i in range(n):
-    for j in range(n):
-        if lst[i][j] ==1:
-            cnt.append(bfs(i,j))
-            
-cnt.sort()
-print(len(cnt))
-for coun in cnt:
-    print(coun)
+def dfs(x,y):
+    if x<0 or x>=N or y<0 or y>=N:
+        return False
     
+    if arr[x][y] == 1:
+        global count
+        count+=1 
+        arr[x][y] = 0
+        
+        for i in range(4):
+        
+            ni = x + di[i]
+            nj = y + dj[i]
+            dfs(ni, nj)
+        return True 
+    return False
+  
+    
+
+N =int(input())
+arr =[list(map(int,input().strip())) for _ in range(N)]
+num = []
+di = (0,1,0,-1)
+dj = (1,0,-1,0)
+count = 0
+result= 0
+
+for i in range(N):
+    for j in range(N):
+        if dfs(i,j) :
+            num.append(count)
+            result += 1
+            count =0
+            
+num.sort()
+print(result)
+for i in range(len(num)):
+    print(num[i])
