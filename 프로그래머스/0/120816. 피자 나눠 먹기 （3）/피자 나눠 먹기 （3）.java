36988bd4b4d4e5733a5ca2b3404e03631154a0bc
rryunn <122458948+rryunn@users.noreply.github.com>
2025-06-11 16:51:15 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 69.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int slice, int n) {
+        if(n%slice!=0){
+            return n/slice+1;
+        }else{
+            return n/slice;
+        }
+    }
+}
