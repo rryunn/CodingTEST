865db1837c26d46b2bcd8390531be63744714cc3
rryunn <122458948+rryunn@users.noreply.github.com>
2025-06-11 14:51:39 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n, int k) {
+        if(n<10){return n*12000+k*2000;}
+        else{
+            int service = n/10;
+            return n*12000 + (k-service)*2000;
+        }
+        
+    }
+}
