ef8a6bea8c86c51dd33210b09a0fccc94b7bbc95
rryunn <122458948+rryunn@users.noreply.github.com>
2025-07-04 01:13:11 +0900

    [level 3] Title: 합승 택시 요금, Time: 128.80 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,13 +1,12 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        
         int[][] map = new int[n+1][n+1];
         for(int i=1;i<=n;i++){
-            Arrays.fill(map[i], 200*100_100);
+            Arrays.fill(map[i],200*100_000);
             map[i][i] = 0;
         }
-        for(int[] fare :fares){
+        for(int[] fare : fares){
             map[fare[0]][fare[1]]= fare[2];
             map[fare[1]][fare[0]]= fare[2];
         }
@@ -15,13 +14,14 @@ class Solution {
             for(int i=1;i<=n;i++){
                 for(int j=1;j<=n;j++){
                     if(map[i][j]>map[i][k]+map[k][j]){
-                        map[i][j]=map[i][k]+map[k][j];
+                        map[i][j] = map[i][k] + map[k][j];
                     }
                 }
             }
         }
-        int answer = map[s][a] + map[s][b];
-        for(int i=1;i<=n;i++){
+        int answer = map[s][a]+ map[s][b];
+        for(int i=0;i<=n;i++){
+            
             answer = Math.min(answer, map[s][i]+map[i][a]+map[i][b]);   
         }
         return answer;
