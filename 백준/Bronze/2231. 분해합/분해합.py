18d92c8079a0970a9159684b6792dfee4217c13e
rryunn <122458948+rryunn@users.noreply.github.com>
2026-01-06 09:43:07 +0900

    [Bronze II] Title: 분해합, Time: 1168 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,15 +1,11 @@
 n = int(input())
-t_min =n
-check = False
+t_min =0
 for i in range(1, n+1):
     arr = list(map(int,str(i)))
     t_sum = sum(arr)
     result = t_sum + i
     if result ==n:
-        check = True
-        if i < t_min:
-            t_min = i
-if check == False:
-    t_min = 0
+        t_min = i
+        break
 print(t_min)
     
