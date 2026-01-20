60b13b5fb6ad68f1e8352bdf60f6c19edf429f2c
rryunn <122458948+rryunn@users.noreply.github.com>
2026-01-20 10:00:00 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,13 +1,12 @@
 n = int(input())
-count = 0
-while n >= 0:
-    if n%5 ==0:
+count=0
+while n>=0:
+    if n%5==0:
         count += n//5
         print(count)
         break
-    
-    n = n - 3
-    count +=1
-    
+    n = n-3
+    count+=1
+
 else:
     print(-1)
