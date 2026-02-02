447cf89aa9a9e8a130df93318ed5cd8cd420b0f9
rryunn <122458948+rryunn@users.noreply.github.com>
2026-02-02 16:13:53 +0900

    [Silver V] Title: 집합, Time: 2860 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,32 +1,32 @@
 import sys
-M = int(sys.stdin.readline())
-arr = set() #파이썬에서 집합을 이용할 땐 set
-
-for _ in range(M):
-    str = sys.stdin.readline().strip().split()
-    
-    if len(str)==1:
-        if str[0] == 'all':
-            arr = set([i for i in range(1,21)])
-        elif str[0] == 'empty':
-            arr = set()
-            
+input = sys.stdin.readline
+m = int(input())
+arr= set()
+for _ in range(m):
+    cmd = input().strip()
+    if cmd == 'all':
+        arr = set(range(1,21))
+    elif cmd == 'empty':
+        arr.clear()
     else:
-        s,num = str[0], str[1]
-        num = int(num)
-        if s == 'add':
-            arr.add(num)
-        elif s == 'remove':
-            arr.discard(num) #remove와 discard와의 차이 확인하기
-        
-        elif s == 'check':
-            if num in arr:
-                print(1)
-            else:  
-                print(0)
-        elif s == 'toggle':
-            if num in arr:
-                arr.discard(num)        
+        cmd, v = cmd.split(" ")
+        v = int(v)
+        if cmd ==  "add":
+            arr.add(v)
+        elif cmd == "remove":
+            if v in arr:
+                arr.remove(v)
             else:
-                arr.add(num)
-   
+                continue
+        elif cmd =="check":
+            if v in arr:
+                print(1)
+            else:
+                print(0)
+        elif cmd == "toggle":
+            if v in arr:
+                arr.remove(v)
+            else:
+                arr.add(v)
+
+    
