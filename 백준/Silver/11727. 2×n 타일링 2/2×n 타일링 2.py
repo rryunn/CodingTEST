44c306d1af797f37f38101dc46d3c9a87df08c5f
rryunn <122458948+rryunn@users.noreply.github.com>
2026-02-12 10:02:28 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -1,11 +1,11 @@
 n = int(input())
-
 dp = [0] * 1001
-dp[0] = 1
-dp[1] = 3
-dp[2] = 5
+dp[1] = 1
+dp[2] = 3
+# dp[3] = 5
+# dp[4] = 9
+# dp[5] = 15
 
-for i in range(3, n+1):
-    dp[i] = dp[i-1] + dp [i-2] * 2
-    
-print(dp[n-1] % 10007) #dp 배열 0부터 시작하니까 n-1 인덱스를 출력해야지!
+for i in range(3,n+1):
+    dp[i] = dp[i-1]+dp[i-2]*2
+print(dp[n]%10007)
