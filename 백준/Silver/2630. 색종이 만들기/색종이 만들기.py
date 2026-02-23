3065b9f5107eaa60b68071859b09035c1ae8988d
rryunn <122458948+rryunn@users.noreply.github.com>
2026-02-23 23:50:20 +0900

    [Silver II] Title: 색종이 만들기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,22 +1,26 @@
-N = int(input())
-
-arr = [list(map(int,input().split())) for _ in range(N)]
-result = []
-def sol(x,y,N):
+def again(x,y,n): #(x,y) 기준 nxn 정사각형 한 색인지 확인.
     color = arr[x][y]
-    for i in range(x,x+N):
-        for j in range(y,y+N):
-            if color != arr[i][j]:
-                sol(x,y,N//2)
-                sol(x,y+N//2,N//2)
-                sol(x+N//2,y,N//2)
-                sol(x+ N//2,y+N//2,N//2)
+    for i in range(x,x+n):
+        for j in range(y,y+n):
+            if color != arr[i][j]: # 다른 색 하나라도 있으면 
+                again(x,y,n//2) # 정사각형 4등분해서 쪼갠다잉.
+                again(x,y+n//2,n//2)
+                again(x+n//2,y,n//2)
+                again(x+n//2,y+n//2,n//2)
                 return
-    if color == 0:
-        result.append(0)
+    if color ==0: # 다른색이 없으면 
+        result.append(0) # 추가
     else:
         result.append(1)
 
-sol(0,0,N)
+n = int(input())
+
+arr = []
+for i in range(n):
+    arr.append(list(map(int, input().split())))
+
+result =[]
+
+again(0,0,n)
 print(result.count(0))
 print(result.count(1))
