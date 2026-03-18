2452bb5956e7ec2394109978c5a3235079e5e3ab
rryunn <122458948+rryunn@users.noreply.github.com>
2026-03-18 23:48:30 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 68 ms, Memory: 35196 KB -BaekjoonHub

---
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
@@ -0,0 +1,66 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n, m = map(int,input().split())
+
+board = [list(input().rstrip()) for _ in range(n)]
+visited = []
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def getPos():
+    rx, ry, bx, by = 0,0,0,0
+    for i in range(n):
+        for j in range(m):
+            if board[i][j]=='R':
+                rx, ry = i,j
+            elif board[i][j] == 'B':
+                bx, by = i, j
+    return rx,ry,bx,by
+
+def move(x,y,dx,dy):
+    count =0
+    while board[x+dx][y+dy] != '#' and board[x][y] != "O":
+        x += dx
+        y += dy
+        count+=1
+    return x, y, count
+
+def bfs():
+    rx, ry, bx, by = getPos()
+    
+    q = deque()
+    q.append((rx,ry,bx,by,1))
+    visited.append((rx,ry,bx,by))
+    
+    while q:
+        rx,ry,bx,by,result = q.popleft()
+        
+        if result >10:
+            break
+        for i in range(4):
+            nrx, nry, rcnt = move(rx,ry,dx[i],dy[i])
+            nbx, nby, bcnt = move(bx,by,dx[i],dy[i])
+            
+            if board[nbx][nby] =='O':
+                continue
+            
+            if board[nrx][nry] == "O":
+                print(result)
+                return
+            
+            if nrx == nbx and nry == nby:
+                if rcnt > bcnt:
+                    nrx -= dx[i]
+                    nry -= dy[i]
+                else:
+                    nbx -= dx[i]
+                    nby -= dy[i]
+                    
+            if (nrx,nry,nbx,nby) not in visited:
+                visited.append((nrx,nry,nbx,nby))
+                q.append((nrx,nry,nbx,nby,result+1))
+    print(-1)
+bfs()
