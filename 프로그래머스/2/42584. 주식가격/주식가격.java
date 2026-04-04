0162ca2b8f79ade95263c925ecfef9f131dd7452
rryunn <122458948+rryunn@users.noreply.github.com>
2026-04-05 08:39:15 +0900

    [level 2] Title: 주식가격, Time: 18.77 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,23 +1,21 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] prices) {
-        int n =prices.length;
-        int[] answer = new int[n];
-        Stack<Integer> stack = new Stack<>();
+        int[] answer = new int[prices.length];
         
-        for(int i=0;i<n;i++){
-            while(!stack.isEmpty() && prices[i]<prices[stack.peek()]){
-                int idx = stack.pop();
-                answer[idx] = i-idx;
-            }
-            stack.push(i);
-        }
         
-        while(!stack.isEmpty()){
-            int idx = stack.pop();
-            answer[idx] = n-idx-1;
+        for(int i=0; i<prices.length; i++){
+            int check = prices[i];
+            int count =0;
+                for(int j=i+1;j<prices.length;j++){
+                    if(prices[i]>prices[j]){
+                        count+=1;
+                        break;
+                    }else{
+                        count+=1;
+                    }
+                }
+            answer[i] = count;
         }
         return answer;
     }
-} 
+}
