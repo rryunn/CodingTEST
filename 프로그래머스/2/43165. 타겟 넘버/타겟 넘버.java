4ec81b82e4d95d31294da728315c6c9772ad7e9e
rryunn <122458948+rryunn@users.noreply.github.com>
2026-07-24 15:20:47 +0900

    [level 2] Title: 타겟 넘버, Time: 3.30 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,15 +1,20 @@
 class Solution {
-    public int solution(int[] numbers, int target) {
-        return dfs(numbers,0,target,0);
-    }
-    int dfs(int[] numbers, int index, int target, int cur){
-        if(index==numbers.length){
-            return(cur==target)?1:0;
-        }
+    int result =0;
+    int sum =0;
+    
+    private void bfs(int startNode, int target, int[] numbers, int sum){
         
-        int sum =0;
-        sum+=dfs(numbers,index+1,target,cur+numbers[index]);
-        sum+=dfs(numbers,index+1,target,cur-numbers[index]);
-        return sum;
+        if(startNode == numbers.length-1){
+            if(sum - numbers[startNode] == target) result = result+1;
+            if(sum + numbers[startNode] == target) result = result+1;
+        }else {
+            bfs(startNode+1,target, numbers,sum+numbers[startNode]);
+            bfs(startNode+1, target, numbers, sum-numbers[startNode]);
+            }   
+        }
+
+    public int solution(int[] numbers, int target) {
+        bfs(0, target, numbers, 0);
+        return result;
     }
 }
