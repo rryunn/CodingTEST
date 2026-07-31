52788f449cd88d726992068f53c4be652285f94c
rryunn <122458948+rryunn@users.noreply.github.com>
2026-07-31 16:48:30 +0900

    [level 3] Title: 가장 먼 노드, Time: 17.98 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,43 +1,43 @@
 import java.util.*;
+
 class Solution {
     public int solution(int n, int[][] edge) {
+        Deque<int[]> queue = new ArrayDeque<>();
+        boolean[] visited = new boolean[n+1];
         
         Map<Integer, List<Integer>> graph = new HashMap<>();
         
         for(int i=1;i<=n;i++){
             graph.put(i, new ArrayList<>());
         }
-        
-        for(int[] e: edge){
+        for(int[] e :edge){
             graph.get(e[0]).add(e[1]);
             graph.get(e[1]).add(e[0]);
         }
         
-        Deque<int[]> queue= new ArrayDeque<>();
-        boolean[] visited = new boolean[n+1];
-        
         queue.offer(new int[] {1,0});
         visited[1] = true;
         
-        int count =0;
-        int maxDist = 0;
+        int maxCount =0;
+        int maxDist =0;
+        
         while(!queue.isEmpty()){
             int[] cur = queue.poll();
-            int dist = cur[1];
             int node = cur[0];
+            int dist = cur[1];
             
             if(maxDist<dist){
-                maxDist= dist;
-                count = 1;
-            }else if(maxDist == dist) count++;
+                maxDist = dist;
+                maxCount=1;
+            }else if(maxDist == dist) maxCount++;
             
             for(int next: graph.get(node)){
                 if(visited[next]) continue;
                 
+                queue.offer(new int[] {next,dist+1});
                 visited[next] = true;
-                queue.offer(new int[] {next, dist+1});
             }
         }
-        return count;
+        return maxCount;
     }
 }
