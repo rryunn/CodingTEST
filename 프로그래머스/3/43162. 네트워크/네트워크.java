3d5eee7f5424cdfdcc6ae5c678e039b0efbdf0fa
rryunn <122458948+rryunn@users.noreply.github.com>
2026-07-31 16:32:53 +0900

    [level 3] Title: 네트워크, Time: 0.20 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,27 +1,25 @@
 import java.util.*;
-
 class Solution {
     public int solution(int n, int[][] computers) {
+        int answer=0;
         Deque<Integer> queue = new ArrayDeque<>();
         boolean[] visited = new boolean[n];
         
-        int answer =0;
         
-        for(int i =0;i<n;i++){
+        for(int i=0;i<n;i++){
             
             if(visited[i]) continue;
-            answer++;
             
+            answer++;
             queue.offer(i);
             visited[i] = true;
             
             while(!queue.isEmpty()){
                 int cur = queue.poll();
-                
                 for(int j=0;j<n;j++){
                     if(computers[cur][j]==1 && !visited[j]){
                         queue.offer(j);
-                        visited[j] = true;
+                        visited[j]= true;
                     }
                 }
             }
